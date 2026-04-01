f6c8aadcfe7b2f3b2dcdacfe7358258e06c9982a
Pedro Villafane <tuemailreal@correo.com>
2026-03-31 22:43:53 -0400

    fix(nextjs): remove invalid appDir config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  output: "standalone", // 🔥 importante en Vercel
-
   images: {
     remotePatterns: [
       {
